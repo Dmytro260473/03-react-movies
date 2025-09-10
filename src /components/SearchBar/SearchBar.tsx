8a685dec0b109e9ad21ea4e18ad844ce985de365
Dmitry Stephanik <dmitrystephanik@MacBook-Pro-Dmitry.local>
2025-09-10 22:43:07 +0300

    hv

--- src /components/SearchBar/SearchBar.tsx
+++ src /components/SearchBar/SearchBar.tsx
@@ -1,12 +1,15 @@
 import styles from "./SearchBar.module.css";
 import { toast } from "react-hot-toast";
+import { FormEvent } from "react";
 
 interface SearchBarProps {
   onSubmit: (query: string) => void | Promise<void>;
 }
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
-  const handleFormAction = async (formData: FormData) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
     const query = formData.get("query")?.toString().trim();
 
     if (!query) {
@@ -15,6 +18,7 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
     }
 
     await onSubmit(query);
+    e.currentTarget.reset(); // Очистка инпута после сабмита
   };
 
   return (
@@ -29,7 +33,7 @@ export default function SearchBar({ onSubmit }: SearchBarProps) {
           Powered by TMDB
         </a>
 
-        <form className={styles.form} action={handleFormAction}>
+        <form className={styles.form} onSubmit={handleSubmit}>
           <input
             className={styles.input}
             type="text"
