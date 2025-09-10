e3c9d54463ef9f50bda54186a7159c18479fe02e
Dmitry Stephanik <dmitrystephanik@MacBook-Pro-Dmitry.local>
2025-09-10 18:40:55 +0300

    hv

--- src /components/MovieGrid/MovieGrid.tsx
+++ src /components/MovieGrid/MovieGrid.tsx
@@ -1,5 +1,5 @@
 import type { Movie } from "../../types/movie";
-import css from "./MovieGrid.module.css";
+import styles from "./MovieGrid.module.css";
 
 interface MovieGridProps {
   movies: Movie[];
@@ -7,25 +7,18 @@ interface MovieGridProps {
 }
 
 export default function MovieGrid({ movies, onSelect }: MovieGridProps) {
-  if (movies.length === 0) return null;
-
   return (
-    <ul className={css.grid}>
+    <ul className={styles.grid}>
       {movies.map((movie) => (
-        <li key={movie.id}>
-          <div
-            className={css.card}
-            onClick={() => onSelect(movie)}
-            role="button"
-            tabIndex={0}
-          >
+        <li key={movie.id} onClick={() => onSelect(movie)}>
+          <div className={styles.card}>
             <img
-              className={css.image}
+              className={styles.image}
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
               alt={movie.title}
               loading="lazy"
             />
-            <h2 className={css.title}>{movie.title}</h2>
+            <h2 className={styles.title}>{movie.title}</h2>
           </div>
         </li>
       ))}
